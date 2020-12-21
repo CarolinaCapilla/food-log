bd6dcf4c4c74adecd5c48e3ff86bb9ee8e727866
CarolinaCapilla <carolina.capilla@gmail.com>
2020-12-21 18:51:37 +0000

    added archives system test, all tested and working as expected

--- test/system/archives_test.rb
+++ test/system/archives_test.rb
@@ -1,9 +1,13 @@
 require "application_system_test_case"
 
 class ArchivesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit archives_url
-  #
-  #   assert_selector "h1", text: "Archives"
-  # end
+  setup { @entry = entries(:breakfast) }
+
+  test "visiting the archives index" do
+    visit archives_index_url
+
+    assert_selector "h1", text: "FoodLog"
+    assert_selector "h3", text: "Entries for #{@entry.day}"
+    assert_selector text: "#{@entry.meal_type}"
+  end
 end
